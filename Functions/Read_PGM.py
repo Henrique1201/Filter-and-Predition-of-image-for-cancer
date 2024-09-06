bee273afd9dece45d8e30255b4994dc7cce35285
Henrique1201 <luizhenriqueteixe@outlook.com>
2024-09-05 21:47:14 -0300

    I took a pgm reading test

--- Functions/Read_PGM.py
+++ Functions/Read_PGM.py
@@ -3,9 +3,15 @@ import pandas as pd
 import numpy as np
 
 
-def read_pgm(file_path):
+def read_pgm_test(file_path):
     imagem = cv2.imread(f'{file_path}', cv2.IMREAD_UNCHANGED)
     df = pd.DataFrame(imagem)
 
     print(df.head(20))
     print(df.shape)
+
+    cv2.imshow('Imagem PGM', imagem)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
